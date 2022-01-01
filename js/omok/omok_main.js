4ef9c3bc951ed09fce184c523dfc5f48b9289db4
sudo-G41 <usa7372@gmail.com>
2022-01-01 22:37:45 +0900

    오목 메인 스크립트 test용 예외사항용

---
+++ js/omok/omok_main.js
@@ -0,0 +1,23 @@
+function omok(lineSize){
+    let line = {0:0,9:9,11:11,13:13,19:19}
+    this.lineCount = line[lineSize];
+    // console.log(this.lineCount);
+    thisNuminLine(this.lineCount);
+    this.canvars = document.getElementById("omok_field");
+    this.field = new omok_field(this.canvars,this.lineCount);
+}
+function thisNuminLine(num){
+    console.log("line # "+num);
+    if(num == undefined){
+        alert("!!! 그런 숫자는 없어요!!");
+        alert("어떻게 그런 값을 넣은 건진 모르겠지만");
+        alert("그러진 말아주세요");
+        alert("please don't touch my code... :-(");
+        alert("ghdpdpd dis has");
+        location.href = "/";
+    }
+}
+omok.prototype.test = function() {
+    alert("A~");
+    console.log(this.lineCount);
+}
