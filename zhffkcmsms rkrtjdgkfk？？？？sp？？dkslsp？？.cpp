a4dee700163a78e34491f1c0aa00f150dc989f2f
sudo-G41 <usa7372@gmail.com>
2022-01-02 22:47:14 +0900

    epgpt 허허....

--- zhffkcmsms rkrtjdgkfk？？？？sp？？dkslsp？？.cpp
+++ zhffkcmsms rkrtjdgkfk？？？？sp？？dkslsp？？.cpp
@@ -1,14 +1,55 @@
 #include<iostream>
-#include<set>
-#define QWER(N,n) (((N)-1)>0?((N)-1):(n)-1+(0*((N)=n)))
+#include<string.h>
 
 using namespace std;
-
-int main(){
-	int a,b,c,d,e;
+int as(int a);
+int asd(int a);
+int main(int argc, char** args){
+    int a;
+    cin>>a;
+    a++;
+    bool f=false;
+    if(argc>1){
+        f=strcmp(args[1],"asd")==0;
+    }
+    for(int i=2;i<a;i++){
+        if(f){
+            cout<<"==============================\n";
+            cout<<"num "<<i<<'\n';
+            asd(i);
+            cout<<"==============================\n";
+        }
+        else{
+            as(i);
+        }
+    }
+    return 0;
+}
+int as(int a){
+	int b,c,d,e;
 	bool f;
 	int b_,c_,d_;
-	cin >> a;
+	b_=b=a;
+	c_=c=1;
+	d=0;
+	d_=3;
+	while(d_>1){
+		if((b_/c_)&1){
+			b_=3*b_+c_;
+		}
+		else{
+			while((b_/c_)%2==0)
+				c_=2*c_;
+		}
+		for(d_=b_;d_%2==0;d_/=2);
+	}
+    cout<<a<<"->"<<b_<<"\n";
+	return 0;
+}
+int asd(int a){
+	int b,c,d,e;
+	bool f;
+	int b_,c_,d_;
 	b_=b=a;
 	c_=c=1;
 	d=0;
